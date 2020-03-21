79f1cba8202d89732b3fb73696dc08440574211b
vkaszuba <codeword.vika@gmail.com>
2020-03-21 10:41:27 -0700

    File order shifts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require("mysql");
+const fs = require('fs');
 
 const path = require('path');
 const app = express();
@@ -7,8 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }))
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -18,31 +18,54 @@ var connection = mysql.createConnection({
   database: "noteTaker_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
 
+app.use(express.static('public'));
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-
-app.get("/api/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "./db.json"));
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./noteTaker_db.json"));
 });
-app.post("/api/notes", function(req, res) {
+
+app.post("/api/notes", function (req, res) {
   var newNote = req.body;
-  console.log(newNote);
-  Notes.push(newNote);
-  res.json(newNote);
+  fs.readFile(__dirname + "/noteTaker_db.json", (err, data) => {
+    if (err) throw err
+    const notes = JSON.parse(data)
+    newNote.id = notes.length + 1
+    notes.push(newNote)
+
+    fs.writeFile(__dirname + "/noteTaker_db.json", JSON.stringify(notes), (err, data) => {
+      if (err) throw err
+      res.json(newNote);
+    })
+  })
 });
 
-app.use(express.static('public'));
+app.delete('/api/notes/:noteId', function (req, res) {
+  const id = req.params.noteId
+  fs.readFile(__dirname + "/noteTaker_db.json", (err, data) => {
+    if (err) throw err
+    const notes = JSON.parse(data)
+
+
+    fs.writeFile(__dirname + "/noteTaker_db.json", JSON.stringify(notes), (err, data) => {
+      if (err) throw err
+      res.json(newNote);
+    })
+  })
+  res.send('ok')
+})
+
+app.get("*", function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(PORT);
