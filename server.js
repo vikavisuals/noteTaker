a1e4eed05db2d165d1c77babdb495c64d7bc8d43
vkaszuba <codeword.vika@gmail.com>
2020-03-21 11:59:46 -0700

    Adding comments

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+// Paths and NPM stuffs required in this file
 const express = require('express');
 const mysql = require("mysql");
 const fs = require('fs');
-
 const path = require('path');
 const app = express();
 
+// Dynamic port
 const PORT = process.env.PORT || 3000;
 
+// Needed for 'body' to work
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+// Establishing MySQL connection
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -18,54 +21,62 @@ var connection = mysql.createConnection({
   database: "noteTaker_db"
 });
 
+// Announcing connection ID
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
 
+// Something IDK
 app.use(express.static('public'));
 
+// GET index HTML
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + 'public/index.html');
+});
+
+// GET notes HTML
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
+// GET notes JSON info
 app.get("/api/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./noteTaker_db.json"));
 });
 
+// Adds a new note to the DB file
 app.post("/api/notes", function (req, res) {
-  var newNote = req.body;
+  var note = req.body;
   fs.readFile(__dirname + "/noteTaker_db.json", (err, data) => {
     if (err) throw err
     const notes = JSON.parse(data)
-    newNote.id = notes.length + 1
-    notes.push(newNote)
+    note.id = notes.length + 1
+    notes.push(note)
 
     fs.writeFile(__dirname + "/noteTaker_db.json", JSON.stringify(notes), (err, data) => {
       if (err) throw err
-      res.json(newNote);
+      res.json(note);
     })
   })
 });
 
-// app.delete('/api/notes/:noteId', function (req, res) {
-//   const id = req.params.noteId
-//   fs.readFile(__dirname + "/noteTaker_db.json", (err, data) => {
-//     if (err) throw err
-//     const notes = JSON.parse(data)
 
+// Deletes a note by ID, not working right now
+app.delete('/api/notes/:noteId', function (req, res) {
+  const id = req.params.noteId;
+  fs.readFile(__dirname + "/noteTaker_db.json", (err, data) => {
+    if (err) throw err
+    const notes = JSON.parse(data)
 
-//     fs.writeFile(__dirname + "/noteTaker_db.json", JSON.stringify(notes), (err, data) => {
-//       if (err) throw err
-//       res.json(newNote);
-//     })
-//   })
-//   res.send('ok')
-// })
+    fs.writeFile(__dirname + "/noteTaker_db.json", JSON.stringify(notes), (err, data) => {
+      if (err) throw err
+      res.json(note);
+    })
+  })
+  res.send('ok')
+})
 
-app.get("*", function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
+// Express listening for port
 app.listen(PORT);
