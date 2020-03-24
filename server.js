1bce02e4225908d37083234f33ed0c4645920404
vkaszuba <codeword.vika@gmail.com>
2020-03-23 18:15:54 -0700

    Fixing delete button and how IDs generate

--- server.js
+++ server.js
@@ -9,58 +9,60 @@ const PORT = process.env.PORT || 3000;
 
 // Needed for 'body' to work
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 
 // Something IDK
 app.use(express.static('public'));
 
-
-// GET index HTML
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
 // GET notes HTML
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
 // GET notes JSON info
-app.get("/api/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./noteTaker_db.json"));
+app.get("/api/notes", function(req, res) {
+  fs.readFile(__dirname + "/noteTaker_db.json", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    for (var i = 0; i < notes.length; i++) {
+      notes[i].id = i;
+    };
+    res.json(notes);
+  });
 });
 
 // Adds a new note to the DB file
-app.post("/api/notes", function (req, res) {
-  var note = req.body;
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
   fs.readFile(__dirname + "/noteTaker_db.json", (err, data) => {
-    if (err) throw err
-    const notes = JSON.parse(data)
-    note.id = notes.length + 1
-    notes.push(note)
-
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
     fs.writeFile(__dirname + "/noteTaker_db.json", JSON.stringify(notes), (err, data) => {
-      if (err) throw err
-      res.json(note);
+      if (err) throw err;
+      res.json(newNote);
     })
   })
 });
 
-
 // Deletes a note by ID, not working right now
 app.delete('/api/notes/:noteId', function (req, res) {
-  const id = req.params.noteId
+  const id = req.params.noteId;
   fs.readFile(__dirname + "/noteTaker_db.json", (err, data) => {
-    if (err) throw err
-    const notes = JSON.parse(data)
-
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.splice(id, 1);
     fs.writeFile(__dirname + "/noteTaker_db.json", JSON.stringify(notes), (err, data) => {
-      if (err) throw err
-      res.json(newNote);
+      if (err) throw err;
+      res.send("ok");
     })
   })
-  res.send('ok')
 })
+
+// GET index HTML
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 // Express listening for port
 app.listen(PORT);
